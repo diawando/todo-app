3dc1cce7006277276b2f6574474c1a414e2fc383
diawando <abduldiawade777@gmail.com>
2025-03-03 19:01:47 +0000

    ajout du provider

--- src/Provider/TodoContext.jsx
+++ src/Provider/TodoContext.jsx
@@ -3,7 +3,69 @@ import React, { createContext, useState, useContext } from 'react';
 // Création du contexte
 const TodoContext = createContext();
 
-// Hook personnalisé pour utiliser le contexte
-export function useTodoContext(){
-     return useContext(TodoContext);
-}
+export const TodoProvider = ({ children }) => {
+      const [todos, setTodos] = React.useState([]);
+
+      // Récuperer les todos depuis localStorage au montage
+      React.useEffect(() => {
+            const storedTodos = localStorage.getItem("todos");
+            if (storedTodos) {
+                  setTodos(JSON.parse(storedTodos));
+            }
+      }, []);
+
+      // Mettre à jour localStorage lorsque les todos changent
+      React.useEffect(() => {
+           localStorage.setItem("todos", JSON.stringify(todos));
+      }, [todos]);
+
+      // Ajouter un nouveau todo
+      const addTodo = (title) => {
+         const newTodo = {
+             id: self.crypto.randomUUID(),
+             title,
+             is_completed: false,
+         };
+         setTodos((prevTodos) => [...prevTodos, newTodo]);
+      };
+
+      // Supprimer un todo
+      const deleteTodo = (id) =>{
+          setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+      };
+
+      // Editer un todo
+      const editTodo = (id, newTitle) => {
+         setTodos((prevTodos) => 
+            prevTodos.map((todo) =>
+                 todo.id === id ? { ...todo, title: newTitle } : todo
+            )
+        );
+      };
+
+      // Marquer un todo comme terminé ou non terminé
+      const toggleComplete = (id) => {
+          setTodos((prevTodos) => 
+             prevTodos.map((todo) => 
+               todo.id === id 
+                   ? { ...todo, is_completed: !todo.is_completed}  
+                   : todo
+            )
+         );
+      };
+
+      // Valeur du contexte
+      const value = {
+          todos, 
+          addTodo,
+          deleteTodo,
+          editTodo,
+          toggleComplete,
+      };
+
+      return (
+           <TodoContext.Provider value={value}>{children}</TodoContext.Provider>
+      );
+};
+
+export const useTodoContext = () => React.useContext(TodoContext);
